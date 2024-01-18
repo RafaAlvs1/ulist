f762e951985c59b476df8e8c832608da19a1294c
Rafa Alves <rafaalvesum@gmail.com>
2024-01-18 17:17:40 -0400

    feat: Pesquisa de tarefa na lista

---
+++ lib/features/todo/pages/todo/widgets/search_textfield.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:ulist_project/core/widgets.dart';
+
+class SearchTextField extends StatefulWidget {
+  final TextEditingController? controller;
+  final Function(String text)? onChanged;
+
+  const SearchTextField({
+    super.key,
+    this.controller,
+    this.onChanged,
+  });
+
+  @override
+  State<SearchTextField> createState() => _SearchTextFieldState();
+}
+
+class _SearchTextFieldState extends State<SearchTextField> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(
+      controller: _controller,
+      prefixIcon: const Icon(Icons.search),
+      placeholder: 'Procurar tarefa...',
+      onChanged: widget.onChanged,
+      textInputAction: TextInputAction.done,
+      onFieldSubmitted: widget.onChanged,
+      suffixIcon: ValueListenableBuilder(
+        valueListenable: _controller,
+        builder: (_, value, child) {
+          if (_controller.text.isEmpty) {
+            return Container(width: 1.0);
+          }
+
+          return IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              FocusScope.of(context).requestFocus(FocusNode());
+              _controller.clear();
+              widget.onChanged?.call('');
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/features/todo/pages/todo/widgets/todo_list_view.dart
+++ lib/features/todo/pages/todo/widgets/todo_list_view.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ulist_project/core/router.dart';
 import 'package:ulist_project/features/todo.dart';
 
+import 'search_textfield.dart';
+
 class TodoListView extends StatefulWidget {
   const TodoListView({Key? key}) : super(key: key);
 
@@ -11,41 +13,64 @@ class TodoListView extends StatefulWidget {
 }
 
 class _TodoListViewState extends State<TodoListView> {
+  String searchText = '';
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: context.read<TodoListCubit>().todos(),
-      builder: (_, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: Container(
-              margin: const EdgeInsets.all(16.0),
-              child: const CircularProgressIndicator.adaptive(),
-            ),
-          );
-        }
+    return Column(
+      children: [
+        SearchTextField(
+          onChanged: (text) {
+            setState(() => searchText = text);
+          },
+        ),
+        Expanded(
+          child: StreamBuilder(
+            stream: context.read<TodoListCubit>().todos(),
+            builder: (_, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(16.0),
+                    child: const CircularProgressIndicator.adaptive(),
+                  ),
+                );
+              }
 
-        final list = snapshot.data ?? [];
-        return Material(
-          color: Colors.white,
-          clipBehavior: Clip.hardEdge,
-          child: ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (_, index) {
-              final todo = list[index];
-              return InkWell(
-                child: _buildItem(todo),
-                onTap: () {
-                  context.push(
-                    Routes.todoView.path,
-                    extra: todo,
-                  );
-                },
+              var list = (snapshot.data ?? []).toList();
+
+              if (searchText.isNotEmpty) {
+                final text = searchText.toLowerCase();
+                list.removeWhere((element) {
+                  final nome = '${element.title?.toLowerCase() ?? ''}'
+                      '${element.description?.toLowerCase() ?? ''}';
+                  return !nome.contains(text);
+                });
+              }
+
+              return Material(
+                color: Colors.white,
+                clipBehavior: Clip.hardEdge,
+                child: ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (_, index) {
+                    final todo = list[index];
+                    return InkWell(
+                      child: _buildItem(todo),
+                      onTap: () {
+                        context.push(
+                          Routes.todoView.path,
+                          extra: todo,
+                        );
+                      },
+                    );
+                  },
+                ),
               );
             },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 
